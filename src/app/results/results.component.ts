af33f0be2bee4dab344a8a5e41febb9e947cf598
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-07-05 21:51:45 +0530

    cards

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../services/http.service";
 
 @Component({
-  selector: 'app-results',
-  templateUrl: './results.component.html',
-  styleUrls: ['./results.component.css']
+  selector: "app-results",
+  templateUrl: "./results.component.html",
+  styleUrls: ["./results.component.css"]
 })
 export class ResultsComponent implements OnInit {
-
-  constructor() { }
+  public dataFetched: {}[];
+  constructor(private _httpService: HttpService) {}
 
   ngOnInit() {
+    this.dataFetched = this._httpService.getAllData();
+    console.log(this.dataFetched);
   }
 
+  getBorderColor(url: string) {
+    return `var(--${
+      ["character", "house", "book"].filter(type =>
+        new RegExp(type, "i").test(url)
+      )[0]
+    })`;
+  }
 }
